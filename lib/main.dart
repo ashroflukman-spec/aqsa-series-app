065c3028f8743bbc35323347b2ac5df41b9cf7c2
ashroflukman-spec <ashroflukman@gmail.com>
2026-03-01 02:31:02 +0800

    Add main.dart placeholder
    Initialize Flutter app with Aqsa Series home screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const AqsaSeriesApp());
+}
+
+class AqsaSeriesApp extends StatelessWidget {
+  const AqsaSeriesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xFF0F0F0F),
+        body: Center(
+          child: Text(
+            'Aqsa Series\nAudio Kitab & Ilmu',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
